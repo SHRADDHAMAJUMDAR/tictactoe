77872af1934a4591798e2bd324d074a79f6bca5a
SHRADDHAMAJUMDAR <128743516+SHRADDHAMAJUMDAR@users.noreply.github.com>
2023-12-22 19:36:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -300,7 +300,7 @@ function myfunc() {
   
 // Function to reset game 
 function myfunc_2() { 
-    location.reload(); 
+    window.reload(); 
     b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = ''; 
 } 
   
